02d2a3eb0e1de561047d29b71c9bcbd9988bb9e6
shubham varma <shubhamvarma793@gmail.com>
2024-02-17 19:29:08 +0530

    update app.js and clodinary.js

--- src/utils/clodinary.js
+++ src/utils/clodinary.js
@@ -1,5 +1,7 @@
 import { v2 as cloudinary } from 'cloudinary';
 import fs from 'fs'; // Fixed import syntax
+import dotenv from "dotenv";
+dotenv.config();
 
 // Cloudinary configuration
 cloudinary.config({ 
